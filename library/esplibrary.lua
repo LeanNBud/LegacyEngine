dc9e08af2758e70a16de14306e1ed4fab29c68ec
LeanNBud <50553549+LeanNBud@users.noreply.github.com>
2022-11-27 14:13:11 -0800

    Update esplibrary.lua

--- library/esplibrary.lua
+++ library/esplibrary.lua
@@ -78,6 +78,20 @@ local module = {
             ShowDistance = 1500,
             EnemyColor = Color3.new(255, 0, 0),
         };
+        aotevoplresp = {
+            Enabled = false,
+            Box = false,
+            Filledbox = false,
+            Info = false,
+            Snaplines = true,
+            Name = false,
+            Distance = false,
+            Healthbox = false,
+            FilledOpacity = 1,
+            ShowDistance = 1500,
+            EnemyColor = Color3.new(255, 0, 0),
+            plrinfo = false,
+        };
     },
 };
 
@@ -895,6 +909,153 @@ function module.aotevo(v)
                     esp.Line["Snapline"].From = Vector2.new(dwCamera.ViewportSize.X/2, 120)
                     esp.Line["Snapline"].To = Vector2.new(math.floor(rootPos.X), math.floor(rootPos.Y))
                     changedrawingcolor(module.Visual.aotevoesp.EnemyColor)
+                else
+                    toggledrawing(false)
+                end
+            else
+                toggledrawing(false)
+                if module.playerexists(v, "universal") == false then
+                    --break
+                end
+            end
+        end
+    end)
+end
+function module.aotevoplrs(v)
+    local esp = {
+        Box = {Filledbox = Drawing.new("Square"), Outline = Drawing.new("Square"), Main = Drawing.new("Square"), HealthboxOutline = Drawing.new("Square"), Healthbox = Drawing.new("Square")},
+        Text = {Distance = Drawing.new("Text"), Name = Drawing.new("Text"), plrinfo = Drawing.new("Text")},
+        Line = {Snapline = Drawing.new("Line")}
+    };
+
+    for index, v in pairs(esp.Box) do
+        v.Visible = false
+        v.Position = Vector2.new(20, 20);
+        v.Size = Vector2.new(20, 20); -- pixels offset from .Position
+        v.Color = Color3.fromRGB(0, 0, 0);
+        v.Filled = false;
+        v.Transparency = 0.9;
+        v.Thickness = 1
+    end
+
+    for index, v in pairs(esp.Line) do
+        v.From = Vector2.new(20, 20); -- origin
+        v.To = Vector2.new(50, 50); -- destination
+        v.Color = Color3.new(0,0,0);
+        v.Thickness = 1;
+        v.Transparency = 0.9;
+        v.Visible = false
+    end
+
+    esp.Box.HealthboxOutline.Thickness = 2
+    esp.Box.Outline.Thickness = 2
+    esp.Box.Healthbox.Filled = true
+    esp.Box.HealthboxOutline.Filled = true
+
+    for index, v in pairs(esp.Text) do
+        v.Text = ""
+        v.Color = Color3.new(1, 1, 1)
+        v.OutlineColor = Color3.new(0, 0, 0)
+        v.Center = true
+        v.Outline = true
+        v.Position = Vector2.new(100, 100)
+        v.Size = 15
+        v.Font = 1 -- 'UI', 'System', 'Plex', 'Monospace'
+        v.Transparency = 0.9
+        v.Visible = false
+    end
+
+    local function toggledrawing(value)
+        for index, v in pairs(esp.Box) do
+            v.Visible = value
+        end
+        for index, v in pairs(esp.Text) do
+            v.Visible = value
+        end
+        for index, v in pairs(esp.Line) do
+            v.Visible = value
+        end
+    end
+
+    local function changedrawingcolor(color)
+        esp.Box["Main"].Color = color
+        esp.Box["Healthbox"].Color = color
+        for i,v in pairs(esp.Text) do
+            v.Color = color
+        end
+        for i,v in pairs(esp.Line) do
+            v.Color = color
+        end
+    end
+
+    task.spawn(function()
+        while task.wait() do
+            if v ~= nil and v:FindFirstChild("Humanoid") ~= nil and v:FindFirstChild("Head") ~= nil and v:FindFirstChild("HumanoidRootPart") ~= nil and v.Humanoid.Health > 0 then --  and v.Humanoid.RigType == Enum.HumanoidRigType.R6
+                local displayEsp = v
+                if displayEsp then
+                    local _,onscreen = dwCamera:WorldToScreenPoint(v.HumanoidRootPart.Position)
+                    displayEsp = onscreen
+                end
+
+                local orientation, sizee = v:GetBoundingBox()
+                local width = (dwCamera.CFrame - dwCamera.CFrame.p) * Vector3.new((math.clamp(sizee.X, 1, 10) + 0.5) / 2, 0, 0)
+                local height = (dwCamera.CFrame - dwCamera.CFrame.p) * Vector3.new(0, (math.clamp(sizee.X, 1, 10) + 2) / 2, 0)
+                width = math.abs(dwCamera:WorldToViewportPoint(orientation.Position + width).X - dwCamera:WorldToViewportPoint(orientation.Position - width).X)
+                height = math.abs(dwCamera:WorldToViewportPoint(orientation.Position + height).Y - dwCamera:WorldToViewportPoint(orientation.Position - height).Y)
+                local size = Vector2.new(math.floor(width), math.floor(height))
+                size = Vector2.new(size.X % 2 == 0 and size.X or size.X + 1, size.Y % 2 == 0 and size.Y or size.Y + 1)
+                local rootPos = dwCamera:WorldToViewportPoint(v.HumanoidRootPart.Position)
+                local magnitude = (v.HumanoidRootPart.Position - dwCamera.CFrame.p).Magnitude
+
+                local TL = dwCamera:WorldToViewportPoint(v.HumanoidRootPart.CFrame * CFrame.new(-3,3,0).p)
+                local TR = dwCamera:WorldToViewportPoint(v.HumanoidRootPart.CFrame * CFrame.new(3,3,0).p)
+                local BL = dwCamera:WorldToViewportPoint(v.HumanoidRootPart.CFrame * CFrame.new(-3,-3,0).p)
+                local BR = dwCamera:WorldToViewportPoint(v.HumanoidRootPart.CFrame * CFrame.new(3,-3,0).p)
+
+                if module.Visual.aotevoplresp.Enabled and displayEsp and magnitude < module.Visual.aotevoplresp.ShowDistance then
+                    --Filledbox
+                    esp.Box["Filledbox"].Visible = module.Visual.aotevoplresp.Filledbox and module.Visual.aotevoplresp.Box
+                    esp.Box["Filledbox"].Size = size
+                    esp.Box["Filledbox"].Filled = module.Visual.aotevoplresp.Filledbox
+                    esp.Box["Filledbox"].Transparency = module.Visual.aotevoplresp.FilledOpacity
+                    esp.Box["Filledbox"].Position = Vector2.new(math.floor(rootPos.X), math.floor(rootPos.Y)) - (esp.Box["Main"].Size / 2)
+
+                    --Box
+                    esp.Box["Outline"].Visible = module.Visual.aotevoplresp.Box
+                    esp.Box["Outline"].Size = size
+                    esp.Box["Outline"].Position = Vector2.new(math.floor(rootPos.X), math.floor(rootPos.Y)) - (esp.Box["Outline"].Size / 2)
+                    esp.Box["Main"].Visible = module.Visual.aotevoplresp.Box
+                    esp.Box["Main"].Size = size
+                    esp.Box["Main"].Position = Vector2.new(math.floor(rootPos.X), math.floor(rootPos.Y)) - (esp.Box["Main"].Size / 2)
+
+                    --Healthbox
+                    esp.Box["HealthboxOutline"].Visible = module.Visual.aotevoplresp.Healthbox
+                    esp.Box["HealthboxOutline"].Size = Vector2.new(1, size.Y * (1-((v.Humanoid.MaxHealth - v.Humanoid.Health) / v.Humanoid.MaxHealth)))
+                    esp.Box["HealthboxOutline"].Position = Vector2.new(math.floor(rootPos.X) - 5, math.floor(rootPos.Y) + (size.Y - math.floor(esp.Box["HealthboxOutline"].Size.Y))) - size / 2
+                    esp.Box["Healthbox"].Visible = module.Visual.aotevoplresp.Healthbox
+                    esp.Box["Healthbox"].Size = Vector2.new(1, size.Y * (1-((v.Humanoid.MaxHealth - v.Humanoid.Health) / v.Humanoid.MaxHealth)))
+                    esp.Box["Healthbox"].Position = Vector2.new(math.floor(rootPos.X) - 5, math.floor(rootPos.Y) + (size.Y - math.floor(esp.Box["Healthbox"].Size.Y))) - size / 2
+
+                    --Nameds
+                    esp.Text["Name"].Visible = module.Visual.aotevoplresp.Name
+                    esp.Text["Name"].Position = Vector2.new(math.floor(rootPos.X), math.floor(rootPos.Y) - size.Y / 2 - 16)
+                    esp.Text["Name"].Text = v.Name.." | ".."Level: "..v.Progression.Level.Value.." | Prestige: "..v.Progression.Prestige.Value
+
+                    --extra plr info
+                    esp.Text["plrinfo"].Visible = module.Visual.aotevoplresp.plrinfo
+                    esp.Text["plrinfo"].Position = Vector2.new(math.floor(rootPos.X), math.floor(rootPos.Y) - size.Y / 2 - 16)
+                    esp.Text["plrinfo"].Text = "Level: "..v.Progression.Level.Value.."\n"
+
+                    --Distance
+                    esp.Text["Distance"].Visible = module.Visual.aotevoplresp.Distance
+                    esp.Text["Distance"].Position = Vector2.new(math.floor(rootPos.X),math.floor(rootPos.Y + height * 0.5))
+                    esp.Text["Distance"].Text = tostring(math.ceil(magnitude)).." studs"
+
+                    --Snapline
+                    esp.Line["Snapline"].Visible = module.Visual.aotevoplresp.Snaplines
+                    esp.Line["Snapline"].From = Vector2.new(dwCamera.ViewportSize.X/2, 120)
+                    esp.Line["Snapline"].To = Vector2.new(math.floor(rootPos.X), math.floor(rootPos.Y))
+                    changedrawingcolor(module.Visual.aotevoplresp.EnemyColor)
                 else
                     toggledrawing(false)
                 end
